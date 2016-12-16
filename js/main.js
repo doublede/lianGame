e77684821444f1d1e9cf94e9515b74668fe00aea
德德 <549089520@qq.com>
2016-12-16 20:30:14 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,12 @@ var hover1Column,hover1Row;
 var hover2Column,hover2Row;
 
 var arr1=new Array();
+var arr2=new Array();
 var index;
+
+var refresh;
+
+var score=0;
 
 window.onload=game;
 
@@ -40,17 +45,39 @@ function game(){
 function init(){
     can1=document.getElementById("canvas1");
     can2=document.getElementById("canvas2");
+    refresh=document.getElementById("refresh");
     if(can1.getContext){
         ctx1=can1.getContext("2d");
         ctx2=can2.getContext("2d");
     }
 
 
-    can2.addEventListener("click",onclick,false);//给canvas2添加鼠标事件
+    can2.addEventListener("click",squareOnclick,false);//给canvas2添加鼠标事件
     
     canWidth=can1.width;
     canHeight=can1.height;
-    createOderArray(0,69,arr1);
+
+
+   // createOderArray(0,107,arr1);
+   for(var i=0;i<9;i++){
+       for(var j=0;j<12;j++){
+           if(i!=0 && i!=8 && j!=0 && j!=11){
+               var lc1=i*12+j;
+               arr1.push(lc1);
+           }
+       }
+	}
+
+    for(var i=0;i<9;i++){
+       for(var j=0;j<12;j++){
+           if(i==0 || i==8 || j==0 || j==11){
+               var lc2=i*12+j;
+               arr2.push(lc2);
+           }
+       }
+	}
+
+
     body=document.getElementsByTagName("body")[0];
     bodyWidth=body.scrollWidth;
     canMarginLeft=(bodyWidth-canWidth)/2;
@@ -63,7 +90,7 @@ function init(){
     inits();
     
 
-    for(var i=0;i<14;i++){
+    for(var i=0;i<19;i++){
         squareNo[i]=new Image();
         squareNo[i].src="./src/square"+i+".jpg";
     }
@@ -75,8 +102,7 @@ function init(){
     hover1x=hover1y=hover2x=hover2y=100;
 
     
-  
-	
+
 
 
 
@@ -93,9 +119,11 @@ function gameLoop(){
 
     drawBackground();
     
-    for(var i=0;i<70;i++){
+    for(var i=0;i<108;i++){
         square[i].draw();
     }
+
+    drawScore();
 
     var gameOver=checkGameOver();
     if(gameOver){
@@ -112,52 +140,55 @@ function drawBackground(){
 }
 
 
-function onclick(event){
+function squareOnclick(event){
     var e=event || window.event;
      if(hover1x==100){
         var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
         var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
         hover1x=e.pageX || e.clientX+scrollX;
         hover1y=e.pageY || e.clientY+scrollY;
-        hover1Column=Math.floor((hover1x-canMarginLeft-50)/70);//[0,9]
-        hover1Row=Math.floor((hover1y-canMarginTop-100)/70);//[0,6]
-        index=foundIndex(hover1Row*10+hover1Column);
+        hover1Column=Math.floor((hover1x-canMarginLeft-50)/70)+1;//[0,9]
+        hover1Row=Math.floor((hover1y-canMarginTop-100)/70)+1;//[0,6]
+        index=foundIndex(hover1Row*12+hover1Column);
         square[index].hover=true;
     }else if(hover2x==100){
         hover2x=e.pageX || e.clientX+scrollX;
         hover2y=e.pageY || e.clientY+scrollY;
-        hover2Column=Math.floor((hover2x-canMarginLeft-50)/70);//[0,9]
-        hover2Row=Math.floor((hover2y-canMarginTop-100)/70);//[0,6]
-        index=foundIndex(hover2Row*10+hover2Column);
+        hover2Column=Math.floor((hover2x-canMarginLeft-50)/70)+1;//[0,9]
+        hover2Row=Math.floor((hover2y-canMarginTop-100)/70)+1;//[0,6]
+        index=foundIndex(hover2Row*12+hover2Column);
         square[index].hover=true;
     }else{
         hover1x=e.pageX || e.clientX+scrollX;
         hover1y=e.pageY || e.clientY+scrollY;
-        hover1Column=Math.floor((hover1x-canMarginLeft-50)/70);
-        hover1Row=Math.floor((hover1y-canMarginTop-100)/70);
+        hover1Column=Math.floor((hover1x-canMarginLeft-50)/70)+1;
+        hover1Row=Math.floor((hover1y-canMarginTop-100)/70)+1;
         hover2x=100;
         hover2y=100;
-        index=foundIndex(hover1Row*10+hover1Column);
+        index=foundIndex(hover1Row*12+hover1Column);
         square[index].hover=true;
     }
     if(hover1x!=100 && hover2x != 100){
         //判断两图是否可消除
         var result=samePic(hover1Row,hover1Column,hover2Row,hover2Column);
         if(result){
-            var aLocation=hover1Row*10+hover1Column;
-            var bLocation=hover2Row*10+hover2Column;
+            var aLocation=hover1Row*12+hover1Column;
+            var bLocation=hover2Row*12+hover2Column;
             if(square[foundIndex(aLocation)].alive && square[foundIndex(bLocation)].alive){
                 square[foundIndex(aLocation)].alive=false;
                 square[foundIndex(bLocation)].alive=false;
+                score += 10;
             }
                                
         }else{
-            for(var i=0;i<70;i++){
+            for(var i=0;i<108;i++){
         square[i].hover=false;
         }
         }     
      
 
     }
+ 
    
 }
+
